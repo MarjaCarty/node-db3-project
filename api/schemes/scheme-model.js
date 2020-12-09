79f1ee6ee5d001328a1ab8e259f59f5b1b67aa50
MarjaCarty <marja.d.carty@gmail.com>
2020-12-09 14:41:20 -0600

    finish half of db helpers

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,27 @@
 // scheme-model
+const db = require("../../data/db-config");
+
+module.exports = {
+  find() {
+    return db("schemes");
+  },
+  findById(id) {
+    return db("schemes").where({ id }).first();
+  },
+  findSteps(id) {
+    // select *
+    // from schemes sc
+    // join steps st
+    //   on sc.id = st.scheme_id
+    // where sc.id = id
+    // order by step_number
+
+    return db("schemes as sc")
+      .where("sc.id", id)
+      .join("steps as st", "sc.id", "st.scheme_id")
+      .orderBy("st.step_number");
+  },
+  add(scheme) {},
+  update(changes, id) {},
+  remove(id) {},
+};
